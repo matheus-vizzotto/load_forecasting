440a5efd29a66b3c9136a8b1ce52c3f0ffa94410
Matheus Vizzotto dos Santos <matheus.vizzotto515@gmail.com>
2023-02-28 21:11:21 -0300

    data

--- prod/scraper.py
+++ prod/scraper.py
@@ -1,71 +1,85 @@
 import pandas as pd
 import requests
 
-def download_carga_horaria(ano_inicio: int, ano_fim: int, printer: bool=False) -> pd.DataFrame:
-    """Função para fazer download dos dados de carga elétrica por subsistema no período de referência em base horária.
-       Página: https://dados.ons.org.br/dataset/curva-carga
-
-    Args:
-        ano_inicio (int): ano inicial de extração.
-        ano_fim (int): ano final de extração.
-        printer(bool): rastreia ano que está sendo extraído com print em tela.
-
-    Returns:
-        pd.DataFrame: Pandas DataFrame com dados de carga horária entre ano_inicio e ano_fim, tendo como índice a data-hora.
+class ons_data:
+    """Classe destinada à leitura dos dados de carga da ONS
     """
+    def __init__(self, freq: str, ano_inicio: int, ano_fim: int, idreg: str=None):
+        """
+        Args:
+            freq (str): frequência da série. ["hourly","daily"]
+            ano_inicio (int): ano inicial de extração.
+            ano_fim (int): ano final de extração.
+            idreg (str): sub-região. ['N', 'NE', 'S', 'SE']
+        """
+        self.freq = freq
+        self.ano_inicio = ano_inicio
+        self.ano_fim = ano_fim
+        self.idreg = idreg
+        self.data_dir = "../../../data/"
 
-    url = "https://ons-dl-prod-opendata.s3.amazonaws.com/dataset/curva-carga-ho/CURVA_CARGA_{}.csv"
-    # verificar se anos inicial e final estão disponíveis
-    get0 = requests.get(url.format(ano_inicio)).status_code # verify = False (autenticação)
-    getn = requests.get(url.format(ano_fim)).status_code 
-    if (get0 == 200) and (getn == 200): # 200: página (ano) disponível
-        # concatenar arquivos de cada ano em um único dataframe
-        df = pd.DataFrame()
-        for ano in range(ano_inicio, ano_fim + 1):
-            if printer:
-                print(f"Lendo ano {ano}...")
-            df2 = pd.read_csv(url.format(ano), sep = ";")
-            df = pd.concat([df, df2])
-        df.columns = ["id_reg", "desc_reg", "date", "load_mwmed"]
-        df.loc[:, "date"] = pd.to_datetime(df.loc[:, "date"], format = '%Y-%m-%d %H:%M:%S')
-        df.sort_values(by = "date", inplace = True)
+    def read(self) -> pd.DataFrame:
+        """Função para ler arquivos "csv" já presentes no diretório de dados.
+
+        Args:
+            
+
+        Returns:
+            pd.DataFrame: série de carga elétrica no período entre ano_inicio e ano_fim.
+        """
+        if self.freq == "hourly":
+            path = "".join([self.data_dir,"hourly_load.csv"])
+        elif self.freq == "daily":
+            path = "".join([self.data_dir,"daily_load.csv"])
+        df = pd.read_csv(path)
+        if not self.idreg:
+            idreg = df["id_reg"].unique()
+        else:
+            idreg = [self.idreg]
+        df = df[df["id_reg"].isin(idreg)]
         df.set_index("date", inplace=True)
         return df
-    else:
-       print("Ano não disponível.")
+
+    def update(self, printer=False, write=False) -> pd.DataFrame:
+        """Função para atualizar os arquivos no diretório de dados.
+
+        Args:
+            printer (bool, optional): Informa o progresso do download ano a ano. Defaults to False.
+            write (bool, optional): Escreve o arquivo no diretório de dados. Defaults to False.
+
+        Raises:
+            Exception: Frequência não está na lista de arquivos disponíveis.
+
+        Returns:
+            pd.DataFrame: série atualizada de carga elétrica no período entre ano_inicio e ano_fim.
+        """
+        if self.freq == "hourly":
+            url = "https://ons-dl-prod-opendata.s3.amazonaws.com/dataset/curva-carga-ho/CURVA_CARGA_{}.csv"
+            date_format = "%Y-%m-%d %H:%M:%S"
+        elif self.freq == "daily":
+            url = "https://ons-dl-prod-opendata.s3.amazonaws.com/dataset/carga_energia_di/CARGA_ENERGIA_{}.csv"
+            date_format = "%Y-%m-%d"
+        else:
+            raise Exception("Frequência não reconhecida. Utilize 'hourly' ou 'daily'.")
+        get0 = requests.get(url.format(self.ano_inicio)).status_code # verify = False (autenticação)
+        getn = requests.get(url.format(self.ano_fim)).status_code 
+        if (get0 == 200) and (getn == 200): # 200: página (ano) disponível
+            # concatenar arquivos de cada ano em um único dataframe
+            df = pd.DataFrame()
+            for ano in range(self.ano_inicio, self.ano_fim + 1):
+                if printer:
+                    print(f"Lendo ano {ano}...")
+                df2 = pd.read_csv(url.format(ano), sep = ";")
+                df = pd.concat([df, df2])
+            df.columns = ["id_reg", "desc_reg", "date", "load_mwmed"]
+            df.loc[:, "date"] = pd.to_datetime(df.loc[:, "date"], format = date_format)
+            df.sort_values(by = "date", inplace = True)
+            df.set_index("date", inplace=True)
+            if write:
+                full_path = "".join([self.data_dir,f"{self.freq}_load.csv"])
+                df.to_csv(full_path)
+            return df
+        else:
+            print("Ano não disponível.")
 
 
-def download_carga_diaria(ano_inicio: int, ano_fim: int, printer: bool=False) -> pd.DataFrame:
-    """Função para fazer download dos dados de carga elétrica por subsistema no período de referência em base diária.
-       Página: https://dados.ons.org.br/dataset/carga-energia
-
-    Args:
-        ano_inicio (int): ano inicial de extração.
-        ano_fim (int): ano final de extração.
-        printer(bool): rastreia ano que está sendo extraído com print em tela.
-
-    Returns:
-        pd.DataFrame: Pandas DataFrame com dados de carga diária entre ano_inicio e ano_fim, tendo como índice a data.
-    """
-    url = "https://ons-dl-prod-opendata.s3.amazonaws.com/dataset/carga_energia_di/CARGA_ENERGIA_{}.csv"
-
-    # verificar se anos inicial e final estão disponíveis
-    get0 = requests.get(url.format(ano_inicio)).status_code # verify = False (autenticação)
-    getn = requests.get(url.format(ano_fim)).status_code 
-    if (get0 == 200) and (getn == 200): # 200: página (ano) disponível
-
-        # concatenar arquivos de cada ano em um único dataframe
-        df = pd.DataFrame()
-        for ano in range(ano_inicio, ano_fim + 1):
-            if printer:
-                print(f"Lendo ano {ano}...")
-            df2 = pd.read_csv(url.format(ano), sep = ";")
-            df = pd.concat([df, df2])
-        df.columns = ["id_reg", "desc_reg", "date", "load_mwmed"]
-        df.loc[:, "date"] = pd.to_datetime(df.loc[:, "date"], format = '%Y-%m-%d')
-        df.sort_values(by = "date", inplace = True)
-        df.set_index("date", inplace=True)
-        return df
-    
-    else:
-       print("Ano não disponível.")
