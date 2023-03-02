0657327ea2bf18d09404dbcd559dcb8def2442da
Matheus Vizzotto dos Santos <matheus.vizzotto515@gmail.com>
2023-03-01 22:01:57 -0300

    plotting

--- prod/scraper.py
+++ prod/scraper.py
@@ -9,7 +9,7 @@ class ons_data:
     def __init__(self, freq: str, ano_inicio: int, ano_fim: int, idreg: str=None):
         """
         Args:
-            freq (str): frequência da série. ["hourly","daily"]
+            freq (str): frequência da série. ["h","d"]
             ano_inicio (int): ano inicial de extração.
             ano_fim (int): ano final de extração.
             idreg (str): sub-região. ['N', 'NE', 'S', 'SE']
@@ -20,6 +20,7 @@ class ons_data:
         self.idreg = idreg
         self.data = pd.DataFrame()
         self.missing_dates = []
+        self.datas_estranhas = []
         self.data_dt_inserted = pd.DataFrame()
         self.data_dir = "../../../data/"
 
@@ -32,9 +33,9 @@ class ons_data:
         Returns:
             pd.DataFrame: série de carga elétrica no período entre ano_inicio e ano_fim.
         """
-        if self.freq == "hourly":
+        if self.freq == "h":
             path = "".join([self.data_dir,"hourly_load.csv"])
-        elif self.freq == "daily":
+        elif self.freq == "d":
             path = "".join([self.data_dir,"daily_load.csv"])
         df = pd.read_csv(path, sep=";", decimal=",", parse_dates=["date"])
         if not self.idreg:
@@ -59,10 +60,10 @@ class ons_data:
         Returns:
             pd.DataFrame: série atualizada de carga elétrica no período entre ano_inicio e ano_fim.
         """
-        if self.freq == "hourly":
+        if self.freq == "h":
             url = "https://ons-dl-prod-opendata.s3.amazonaws.com/dataset/curva-carga-ho/CURVA_CARGA_{}.csv"
             date_format = "%Y-%m-%d %H:%M:%S"
-        elif self.freq == "daily":
+        elif self.freq == "d":
             url = "https://ons-dl-prod-opendata.s3.amazonaws.com/dataset/carga_energia_di/CARGA_ENERGIA_{}.csv"
             date_format = "%Y-%m-%d"
         else:
@@ -89,7 +90,7 @@ class ons_data:
         else:
             print("Ano não disponível.")
     
-    def check_date_column(self, _freq: str, printer=False) -> List[dt.datetime]:
+    def check_date_column(self, printer=False) -> List[dt.datetime]:
         """Verifica datas faltantes no intervalo
 
         Args:
@@ -100,10 +101,14 @@ class ons_data:
             List[dt.datetime]: lista de datas faltantes
         """
         date_col = self.data.reset_index()["date"]
-        missing_dates = pd.date_range(date_col.min(), date_col.max(), freq=_freq).difference(date_col)
-        missing_list = missing_dates.to_list()
+        _dt_range = pd.date_range(date_col.min(), date_col.max(), freq=self.freq)
+        missing_dates_ = _dt_range.difference(date_col)
+        missing_list = missing_dates_.to_list()
+        dts_extras = date_col[~(date_col.isin(_dt_range))].to_list()
         if printer:
             print("Datas faltantes:\n", missing_list)
+            print("Datas estranhas:\n", dts_extras)
+        self.datas_estranhas = dts_extras
         self.missing_dates = missing_list
         return missing_list
     
@@ -114,37 +119,24 @@ class ons_data:
         y.loc[:,"date"] = pd.to_datetime(y.loc[:,"date"])
         y.set_index("date", inplace=True)
         y.sort_index(inplace=True)
-        faltantes = check_date_column(y.index, _freq='h')
+        faltantes = check_date_column(y, _freq='h')
         if printer:
             print("Datas faltantes após transformação:", faltantes)
         self.data_dt_inserted = y
         return y
 
-
-def check_date_column(date_col: List[dt.datetime], _freq: str, printer=False) -> List(dt.datetime):
-    """Verifica datas faltantes no intervalo.
-
-    Args:
-        date_col (List[dt.datetime]): coluna de datas.
-        _freq (str): frequência da coluna de datas.
-        printer (bool, optional): traz as datas fatantes em tela. Defaults to False.
-
-    Returns:
-        List(dt.datetime): lista de datas faltantes
-    """
-    missing_dates = pd.date_range(date_col.min(), date_col.max(), freq=_freq).difference(date_col)
-    missing_list = missing_dates.to_list()
-    if printer:
-        print("Datas faltantes:\n", missing_list)
-    return missing_list
-
-def insert_missing_dates(x: pd.DataFrame, missing_dates: list, date_column_name: str):
-    y = x.reset_index()
-    missing = pd.DataFrame(missing_dates, columns=[date_column_name])
-    y = pd.concat([y, missing], ignore_index=True)
-    y.loc[:,"date"] = pd.to_datetime(y.loc[:,"date"])
-    y.set_index("date", inplace=True)
-    y.sort_index(inplace=True)
-    faltantes = check_date_column(y.index, _freq='h')
-    print("Datas faltantes após transformação:", faltantes)
-    return y
+    def get_data_description(self, plot=False):
+        data = self.data_dt_inserted
+        print("Valores vazios:")
+        n_missing = data["load_mwmed"].isna().sum()
+        print(data.isna().sum())
+        print("\nValor mínimo:", data["load_mwmed"].min())
+        print("Valor máximo:", data["load_mwmed"].max())
+        if plot==True:
+            missing_idx = data[data["load_mwmed"].isna()].index
+            fig, ax = plt.subplots(figsize=(15,5))
+            plt.plot(pd.to_datetime(data.index), data["load_mwmed"])
+            for data_faltante in missing_idx:
+                plt.axvline(x=data_faltante, color='red', linestyle='--')
+            plt.title(f"Valores vazios: {n_missing}")
+            plt.show()
