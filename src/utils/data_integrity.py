26de5caada8d798107e7859cb8bf15bf7eedd83e
Matheus Vizzotto dos Santos <matheus.vizzotto515@gmail.com>
2023-04-12 21:15:58 -0300

    di

--- src/utils/data_integrity.py
+++ src/utils/data_integrity.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import os
 
-def check_date_range(data: pd.DataFrame, date_col_name: str, frequency: str, date_is_index: bool):
+def check_date_range(data: pd.DataFrame, date_col_name: str, frequency: str):
     """_summary_
 
     Args:
@@ -11,41 +11,43 @@ def check_date_range(data: pd.DataFrame, date_col_name: str, frequency: str, dat
         frequency (str): ["min", "h", "d", "M"]
         date_is_index (bool): _description_
     """
-    if date_is_index:
-        date_col = data.reset_index()[date_col_name]
-    else:
-        date_col = data[date_col_name]
+    # if date_is_index:
+    #     date_col = data.reset_index()[date_col_name]
+    # else:
+    date_col = data[date_col_name]
     _dt_range = pd.date_range(date_col.min(), date_col.max(), freq=frequency)
     missing_dates_ = _dt_range.difference(date_col).to_list()
     if missing_dates_:
-        return "Datas faltantes: ", missing_dates_
+        return f"Datas faltantes: {len(missing_dates_)}", missing_dates_
     else:
-        return "Sem datas faltantes."
+        return "Sem datas faltantes.", None
     
-def input_missing_dates(data: pd.DataFrame, date_col_name: str, missing_dates_: list, date_is_index: bool) -> pd.DataFrame:
-    """_summary_
+def input_missing_dates(data_: pd.DataFrame, date_col_name: str, freq: str, date_is_index: bool) -> pd.DataFrame:
+    """Verifica se há datas faltantes no dataframe e, no caso positivo, as inclui no original, deixando as
+    demais colunas com NaN.
 
     Args:
-        data (pd.DataFrame): _description_
-        date_col_name (str): _description_
-        date_is_index (bool): _description_
+        data (pd.DataFrame): DataFrame com coluna de data
+        date_col_name (str): Nome da coluna de data
+        date_is_index (bool): A coluna de data está como índice?
 
     Returns:
         pd.DataFrameS: _description_
     """
     if date_is_index:
-        y = data.reset_index()
+        y = data_.reset_index()
     else:
-        y = data
+        y = data_
+    msg, missing_dates_ = check_date_range(data=y, date_col_name=date_col_name, frequency=freq)
     missing = pd.DataFrame(missing_dates_, columns=[f"{date_col_name}"])
     y = pd.concat([y, missing], ignore_index=True).sort_values(by=date_col_name)
-    return y
+    return msg, y
 
 def check_outliers(data: pd.Series, method='desvpad') -> pd.Series:
     if method == 'desvpad':
-            window = 24*7
-            n_std = 1
-            outlier = np.where( (data > data+(n_std*data.rolling(window).std())) | (data < data-(n_std*data.rolling(window).std()),1,0) )
+        window = 24*7
+        n_std = 2
+        outlier = np.where( (data > data+(n_std*data.rolling(window).std())) | (data < data-(n_std*data.rolling(window).std()),1,0) )
     return outlier
 
 # def merge_dataframes(data1: pd.DataFrame, data2: pd.DataFrame, date1: str, date2: str, dt_min, dt_max, freq_='h') -> pd.DataFrame:
