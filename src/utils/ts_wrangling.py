30347f02856a04a03dd78b6cbcb92c0a1354fbef
Matheus Vizzotto dos Santos <matheus.vizzotto515@gmail.com>
2023-04-13 22:39:42 -0300

    Update ts_wrangling.py

--- src/utils/ts_wrangling.py
+++ src/utils/ts_wrangling.py
@@ -2,6 +2,18 @@ import pandas as pd
 import os
 
 def train_test_split(df: pd.DataFrame, test: int, multivariate=False, y_col: str=None) -> pd.DataFrame:
+    """Função para particionar dataframes uni ou multivariados
+
+    Args:
+        df (pd.DataFrame): Dataframe com a série temporal
+        test (int): Tamanho da partição de teste, utilizando os dados mais recentes
+        multivariate (bool, optional): Dados são multivariados? (Necessário incluir o nome da variável resposta em y_col). Defaults to False.
+        y_col (str, optional): Nome da coluna de variável resposta. Defaults to None.
+
+    Returns:
+        pd.DataFrame: 4 ou 2 Dataframes de treino e teste, nessa ordem. Se multivariado,
+        o resultado é "train_x, train_y, test_x, test_y"; se univariado, "train, test".
+    """
     train = df.iloc[:-test,:]
     test = df.iloc[-test:,:]
     if multivariate:
