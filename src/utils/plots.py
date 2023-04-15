dc75e3e8bb3339d19e6e4e1f7d83762d74a7622a
Matheus Vizzotto dos Santos <matheus.vizzotto515@gmail.com>
2023-04-14 21:52:28 -0300

    plotting

--- src/utils/plots.py
+++ src/utils/plots.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 
-def plot_ts(y_: pd.Series, x_: pd.Series, title_=None, save=False):
+def plot_ts(x_: pd.Series, y_: pd.Series, title_=None, save=False):
     """_summary_
 
     Args:
@@ -19,3 +19,23 @@ def plot_ts(y_: pd.Series, x_: pd.Series, title_=None, save=False):
         plt.savefig(f"../figs/{title_}.png")
     else:
         plt.show()
+
+def plot_with_confidence(data: pd.DataFrame, x_: str, y_: str, levels: list=[90],title_=None, save=False):
+    """_summary_
+
+    Args:
+        y_ (pd.DataFrame): Dataframe com observado e ICs
+        x_ (pd.Series): Data
+        levels (list, optional): _description_. Defaults to [90].
+        title_ (_type_, optional): _description_. Defaults to None.
+        save (bool, optional): _description_. Defaults to False.
+    """
+    plt.figure(figsize=(15,5))
+    sns.lineplot(x=data[x_], y=data[y_])
+    for ic in levels:
+        cols = [x for x in data.columns if f"{ic}" in data.columns]
+        print(cols)
+        plt.fill_between(data[x_], cols[0], cols[1], alpha=0.2, label=f"IC -{ic:%}%")
+    plt.legend(bbox_to_anchor = (1.15,1))
+    plt.show()
+
